4aecdd24842fc44541969ddf4ab6a4233c0cd8c7
Herbet Ferreira Rodrigues <herbetferreira@gmail.com>
2019-08-21 08:29:02 -0300

    alterando funcao autoload

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ require 'config.php';
 require 'util/Auth.php';
 
 // Also spl_autoload_register (Take a look at it if you like)
-function __autoload($class) {
+function my_autoload($class) {
 	require LIBS . $class .".php";
 }
+
+spl_autoload_register("my_autoload");
 
 
 /*// Use an autoloader!
